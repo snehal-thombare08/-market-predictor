07b06540b389f09f3cb204853064794a3463ddfe
snehalthombare08 <snehalthombare888@gmail.com>
2026-05-25 10:52:52 +0530

    Fix: disable CI warnings for Vercel build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-﻿import React, { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ReferenceLine } from "recharts";
 
